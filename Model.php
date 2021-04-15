f6174676799e4fa2bff85f1e1efd5faec46f6213
denis909 <denis909@mail.ru>
2021-04-15 15:31:17 +0300

    n/a

--- Model.php
+++ Model.php
@@ -2,9 +2,21 @@
 
 namespace denis909\yii;
 
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\NullLogger;
+
 class Model extends \yii\base\Model implements ModelInterface
 {
 
     use ModelTrait;
+    
+    use LoggerAwareTrait;
+
+    public function __construct($config = [])
+    {
+        parent::__construct($config);
+
+        $this->setLogger(new NullLogger);
+    }
     
 }
